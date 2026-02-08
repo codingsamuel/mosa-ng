c0356f5cf3e534380370724d723cebb0fc927a8b
Samuel Marcelino Moutinho <samifmoutinho@gmail.com>
2026-02-08 21:46:08 +0100

    feat(pipes): add compareFn to filter and find pipe

--- projects/core/src/lib/pipes/filter.pipe.ts
+++ projects/core/src/lib/pipes/filter.pipe.ts
@@ -5,13 +5,13 @@ import { some } from '../utils/commons.util';
 export class FilterPipe implements PipeTransform {
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    public transform<T>(array: T[] | undefined, searchValue: any, searchKey?: keyof T): T[] {
+    public transform<T>(array: T[] | undefined, searchValue: any, searchKey?: keyof T, compareFn: ((item: T, val: T[keyof T]) => boolean) | 'equals' | 'not-equals' = 'equals'): T[] {
         // Return if array is empty or null
         if (!array?.length) {
             return [];
         }
 
-        const foundItems: T[] = array.filter((v: T): boolean => some(v, searchValue, searchKey));
+        const foundItems: T[] = array.filter((v: T): boolean => some(v, searchValue, searchKey, compareFn));
         if (!foundItems.length) {
             return [];
         }
--- projects/core/src/lib/pipes/find.pipe.ts
+++ projects/core/src/lib/pipes/find.pipe.ts
@@ -5,13 +5,13 @@ import { some } from '../utils/commons.util';
 export class FindPipe implements PipeTransform {
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    public transform<T>(array: T[] | undefined, searchValue: any, searchKey?: keyof T): T | null {
+    public transform<T>(array: T[] | undefined, searchValue: any, searchKey?: keyof T, compareFn: ((item: T, val: T[keyof T]) => boolean) | 'equals' | 'not-equals' = 'equals'): T | null {
         // Return if array is empty or null
         if (!array?.length) {
             return null;
         }
 
-        const foundItem: T | undefined = array.find((v: T): boolean => some(v, searchValue, searchKey));
+        const foundItem: T | undefined = array.find((v: T): boolean => some(v, searchValue, searchKey, compareFn));
         if (!foundItem) {
             return null;
         }
